fad465fb10b74c1382b0f6522b7657eba1d1c861
Tom McFarlin <tom@tommcfarlin.com>
2012-08-09 15:09:14 -0400

    Finishing up the final version of the plugin.

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 
 An example plugin used to demonstrate the WordPress Ajax API for a companion article on Envato's WPTuts+ site.
 
+## Features
+
+1. When the plugin is activated, a notification message will appear.
+2. The message will appear throughout the time the plugin is active.
+3. If the user clicks on the 'dismiss' anchor, the message will disappear.
+4. If the user deactivates the plugin, the message will disappear.
+
 ## How It Works
 
-## Features
+* Upon activation, the plugin adds an option to the options table used to track whether or not the user wishes to display the message.
+* When the user clicks on the 'dismiss' anchor, a request is sent to the server via Ajax.
+* The option created during activation is set to false to hide the message.
+* The message will fade out.
+* When the user deactivates the plugin, the data is erased from the database and the process will repeat on activation.
 
 ## Changelog
 
+_1.0 (August 8th, 2012)_
+
+* Completing Ajax functionality
+
+_0.1 (August 7th, 2012)_
+
+* Initial release. Static functionality only - no Ajax.
--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Ajax Notification
 Plugin URI: http://github.com/tommcfarlin/ajax-notification
-Description: An example plugin used to demonstrate the WordPress Ajax API for a companion article on Envato's WPTuts+ site
+Description: An example plugin used to demonstrate the WordPress Ajax API for a companion article on Envato's WPTuts+ site.
 Version: 1.0
 Author: Tom McFarlin
 Author URI: http://tommcfarlin.com
@@ -36,40 +36,59 @@ class Ajax_Notification {
 	 * Initializes the plugin by setting localization, filters, and administration functions.
 	 */
 	function __construct() {
-	
+
+		load_plugin_textdomain( 'ajax-notification', false, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+
 		register_activation_hook( __FILE__, array( &$this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
-		add_action( 'admin_notices', array( &$this, 'display_admin_notice' ) );
+
+		add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
+		
+		// Display the admin notice only if it hasn't been hidden
+	    if( false == get_option( 'hide_ajax_notification' ) ) { 
+		    add_action( 'admin_notices', array( &$this, 'display_admin_notice' ) );
+	    } // end if
+	    
+	    add_action( 'wp_ajax_hide_admin_notification', array( &$this, 'hide_admin_notification' ) );
 
 	} // end constructor
-	
-	/**
-	 * Fired when the plugin is activated.
-	 *
-	 * @params	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
-	 */
-	function activate( $network_wide ) {
-		add_option( 'hide_ajax_notification', false );
-	} // end activate
-	
-	/**
-	 * Fired when the plugin is deactivated.
-	 *
-	 * @params	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog 
-	 */
-	function deactivate( $network_wide ) {
-		delete_option( 'hide_ajax_notification' );
-	} // end deactivate
 	
 	/*--------------------------------------------*
 	 * Core Functions
 	 *---------------------------------------------*/
 	
-	function display_admin_notice() {
+	/**
+	 * Upon activation, add a new option used to track whether or not to display the notification.
+	 */
+	public function activate() {
+		add_option( 'hide_ajax_notification', false );
+	} // end activate
+	
+	/**
+	 * Upon deactivation, removes the option that was created when the plugin was activated.
+	 */
+	public function deactivate() {
+		delete_option( 'hide_ajax_notification' );
+	} // end deactivate
+
+	/**
+	 * Registers and enqueues admin-specific minified JavaScript.
+	 */	
+	public function register_admin_scripts() {
+	
+		wp_register_script( 'ajax-notification-admin', plugins_url( 'ajax-notification/js/admin.min.js' ) );
+		wp_enqueue_script( 'ajax-notification-admin' );
+	
+	} // end register_admin_scripts
+	
+	/**
+	 * Renders the administration notice. Also renders a hidden nonce used for security when processing the Ajax request.
+	 */
+	public function display_admin_notice() {
     	
     	$html = '<div id="ajax-notification" class="updated">';
     		$html .= '<p>';
-    			$html .= 'The Ajax Notification example plugin is active. This message will appear until you choose to <a href="javascript:;" id="dismiss-ajax-notification">dismiss it</a>.';
+    			$html .= __( 'The Ajax Notification example plugin is active. This message will appear until you choose to <a href="javascript:;" id="dismiss-ajax-notification">dismiss it</a>.', 'ajax-notification' );
     		$html .= '</p>';
     		$html .= '<span id="ajax-notification-nonce" class="hidden">' . wp_create_nonce( 'ajax-notification-nonce' ) . '</span>';
     	$html .= '</div><!-- /.updated -->';
@@ -77,6 +96,27 @@ class Ajax_Notification {
     	echo $html;
     	
 	} // end display_admin_notice
+	
+	/**
+	 * JavaScript callback used to hide the administration notice when the 'Dismiss' anchor is clicked on the front end.
+	 */
+	public function hide_admin_notification() {
+		
+		// First, check the nonce to make sure it matches what we created when displaying the message. 
+		// If not, we won't do anything.
+		if( wp_verify_nonce( $_REQUEST['nonce'], 'ajax-notification-nonce' ) ) {
+			
+			// If the update to the option is successful, send 1 back to the browser;
+			// Otherwise, send 0.
+			if( update_option( 'hide_ajax_notification', true ) ) {
+				die( '1' );
+			} else {
+				die( '0' );
+			} // end if/else
+			
+		} // end if
+		
+	} // end hide_admin_notification
   
 } // end class
 
